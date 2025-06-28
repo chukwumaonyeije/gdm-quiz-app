8e9920c802709635c54adcf052ce4f5148304562
chukwumaonyeije <onyeije@gmail.com>
2025-06-28 19:06:52 -0400

    Fix base path for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/postcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // This line is the key!
+  base: '/gdm-quiz-app/', 
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
